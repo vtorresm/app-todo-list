821aec3db233a47139f7068123a3eb703043d5f8
Victor Torres <vatorresm88@gmail.com>
2023-08-10 23:14:21 -0500

    Llamando al metodo showMenu

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-import 'colors';
+require('colors');
+
+const { showMenu } = require('./helpers/messages.js');
 
 console.clear();
 
 const main = async () => {
   console.log('hola mundo'.rainbow);
+
+  showMenu();
 };
 
 main();
