c4bc1745bd1f5d54f7d93386923f501b2fa6e819
Victor Torres <vatorresm88@gmail.com>
2023-08-11 23:16:42 -0500

    Creando el helper showMenu

---
+++ helpers/messages.js
@@ -0,0 +1,32 @@
+require('colors');
+
+const showMenu = () => {
+  return new Promise((resolve, reject) => {
+    console.clear();
+    console.log('================================='.green);
+    console.log('       Select an option'.green);
+    console.log('=================================\n'.green);
+
+    console.log(`${'1.'.green} Create task`);
+    console.log(`${'2.'.green} List tasks`);
+    console.log(`${'3.'.green} List completed tasks`);
+    console.log(`${'4.'.green} List pending tasks`);
+    console.log(`${'5.'.green} Complete task(s)`);
+    console.log(`${'6.'.green} Delete task`);
+    console.log(`${'0.'.green} Exit\n`);
+
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question('Select an option: ', (opt) => {
+      readline.close();
+      resolve(opt);
+    });
+  });
+}
+
+module.exports = {
+  showMenu,
+}
