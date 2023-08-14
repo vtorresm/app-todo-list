27570b67d7f0311ea9158363a62781fc7e207240
Victor Torres <vatorresm88@gmail.com>
2023-08-13 22:14:51 -0500

    Agregando helpers messages

--- helpers/messages.js
+++ helpers/messages.js
@@ -4,29 +4,29 @@ const showMenu = () => {
   return new Promise((resolve, reject) => {
     console.clear();
     console.log('================================='.green);
-    console.log('       Select an option'.green);
+    console.log('   Selecione una opción'.green);
     console.log('=================================\n'.green);
 
-    console.log(`${'1.'.green} Create task`);
-    console.log(`${'2.'.green} List tasks`);
-    console.log(`${'3.'.green} List completed tasks`);
-    console.log(`${'4.'.green} List pending tasks`);
-    console.log(`${'5.'.green} Complete task(s)`);
-    console.log(`${'6.'.green} Delete task`);
-    console.log(`${'0.'.green} Exit\n`);
+    console.log(`${'1.'.green} Crear tarea`);
+    console.log(`${'2.'.green} Listar tareas`);
+    console.log(`${'3.'.green} Listar tareas completadas`);
+    console.log(`${'4.'.green} Listar tareas pendientes`);
+    console.log(`${'5.'.green} Completar tarea(s)`);
+    console.log(`${'6.'.green} Eliminar tarea`);
+    console.log(`${'0.'.green} Salir\n`);
 
     const readline = require('readline').createInterface({
       input: process.stdin,
       output: process.stdout,
     });
 
-    readline.question('Select an option: ', (opt) => {
+    readline.question('Selecione la opción: ', (opt) => {
       readline.close();
       resolve(opt);
     });
   });
-}
+};
 
 module.exports = {
   showMenu,
-}
+};
