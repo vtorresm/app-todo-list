3014e0c955dea00afe5bbe9c724005ddb0d09b15
Victor Torres <vatorresm88@gmail.com>
2023-08-16 13:38:42 -0500

    Cambiando require por type: Module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-require('colors');
+import 'colors';
 
-const { showMenu, pause } = require('./helpers/messages.js');
+//const { inquirerMenu, pause } = require('./helpers/inquirer.js');
+import { inquirerMenu, pause } from './helpers/inquirer.js';
 
 console.clear();
 
@@ -10,10 +11,10 @@ const main = async () => {
   let opt = '';
 
   do {
-    opt = await showMenu();
+    opt = await inquirerMenu();
     console.log({ opt });
 
-    if (opt !== '0') await pause();
+    await pause();
   } while (opt !== '0');
 
   // pause();
