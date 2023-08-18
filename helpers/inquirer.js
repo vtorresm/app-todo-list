45b47ffdbdbcc01f2d7a83b93d58aa5ede13271b
Victor Torres <vatorresm88@gmail.com>
2023-08-17 22:27:37 -0500

    Agregando metodo readInput, listTasksDelete, confirm y showListChecklist

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -25,7 +25,7 @@ const questions = [
       },
       {
         value: '5',
-        name: `${'5.'.green} Completar tarea`,
+        name: `${'5.'.green} Completar tarea(s)`,
       },
       {
         value: '6',
@@ -41,13 +41,13 @@ const questions = [
 
 const inquirerMenu = async () => {
   console.clear();
-  console.log('================================='.green);
-  console.log('   Selecione una opción'.green);
-  console.log('=================================\n'.green);
+  console.log('=========================='.green);
+  console.log('  Seleccione una opción'.white);
+  console.log('==========================\n'.green);
 
-  const opt = await inquirer.prompt(questions);
+  const { option } = await inquirer.prompt(questions);
 
-  return opt;
+  return option;
 };
 
 const pause = async () => {
@@ -59,7 +59,99 @@ const pause = async () => {
     },
   ];
 
+  console.log('\n');
   await inquirer.prompt(question);
 };
 
-export { inquirerMenu, pause };
+const readInput = async (message) => {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      validate(value) {
+        if (value.length === 0) {
+          return 'Por favor ingrese un valor';
+        }
+        return true;
+      },
+    },
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+};
+
+const listTasksDelete = async (tasks = []) => {
+  const choices = tasks.map((task, i) => {
+    const idx = `${i + 1}.`.green;
+
+    return {
+      value: task.id,
+      name: `${idx} ${task.desc}`,
+    };
+  });
+
+  choices.unshift({
+    value: '0',
+    name: '0.'.green + ' Cancelar',
+  });
+
+  const questions = [
+    {
+      type: 'list',
+      name: 'id',
+      message: 'Borrar',
+      choices,
+    },
+  ];
+
+  const { id } = await inquirer.prompt(questions);
+  return id;
+};
+
+const confirm = async (message) => {
+  const question = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message,
+    },
+  ];
+
+  const { ok } = await inquirer.prompt(question);
+  return ok;
+};
+
+const showListChecklist = async (tasks = []) => {
+  const choices = tasks.map((task, i) => {
+    const idx = `${i + 1}.`.green;
+
+    return {
+      value: task.id,
+      name: `${idx} ${task.desc}`,
+      checked: task.completedIn ? true : false,
+    };
+  });
+
+  const question = [
+    {
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Selecciones',
+      choices,
+    },
+  ];
+
+  const { ids } = await inquirer.prompt(question);
+  return ids;
+};
+
+module.exports = {
+  inquirerMenu,
+  pause,
+  readInput,
+  listTasksDelete,
+  confirm,
+  showListChecklist,
+};
