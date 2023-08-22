28617a76d50aedbb01de1e607013fd787d0d95a1
Victor Torres <vatorresm88@gmail.com>
2023-08-22 10:13:15 -0500

    Llamado a los metodos

--- app.js
+++ app.js
@@ -3,71 +3,78 @@ import 'colors';
 import { saveDB, readDB } from './helpers/saveFile.js';
 import {
   inquirerMenu,
-  pause,
-  readInput,
-  listTasksDelete,
-  confirm,
-  showListChecklist,
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+  confirmar,
+  mostrarListadoChecklist,
 } from './helpers/inquirer.js';
-import Tasks from './models/tasks.js';
 
-const main = async () => {
+import Tareas from './models/tasks.js';
+
+const main = async() => {
+
   let opt = '';
-  const tasks = new Tasks();
+  const tareas = new Tareas();
 
-  const tasksDB = readDB();
+  const tareasDB = readDB();
 
-  if (tasksDB) {
-    //TODO: cargar tareas
-    tasks.loadingTasksFromArray(tasksDB);
+  if ( tareasDB ) { // cargar tareas
+      tareas.cargarTareasFromArray( tareasDB );
   }
 
   do {
-    //TODO: Imprimir el menú
-    opt = await inquirerMenu();
+      // Imprimir el menú
+      opt = await inquirerMenu();
 
-    switch (opt) {
-      case '1':
-        //TODO: crear opcion
-        const desc = await readInput('Descripción:');
-        tasks.creatingTask(desc);
-        break;
+      switch (opt) {
+          case '1':
+              // crear opcion
+              const desc = await leerInput('Descripción:');
+              tareas.crearTarea( desc );
+          break;
 
-      case '2':
-        tasks.listedComplete();
-        break;
+          case '2':
+              tareas.listadoCompleto();
+          break;
 
-      case '3': //TODO: listar completadas
-        tasks.listPendingCompleted(true);
-        break;
+          case '3': // listar completadas
+              tareas.listarPendientesCompletadas(true);
+          break;
 
-      case '4': //TODO: listar pendientes
-        tasks.listPendingCompleted(false);
-        break;
+          case '4': // listar pendientes
+              tareas.listarPendientesCompletadas(false);
+          break;
 
-      case '5': //TODO: completado | pendiente
-        const ids = await showListChecklist(tasks.listadoArr);
-        tasks.togglecompleted(ids);
-        break;
+          case '5': // completado | pendiente
+              const ids = await mostrarListadoChecklist( tareas.listadoArr );
+              tareas.toggleCompletadas( ids );
+          break;
 
-      case '6': //TODO: Borrar
-        const id = await listTasksDelete(tasks.listadoArr);
-        if (id !== '0') {
-          const ok = await confirm('¿Está seguro?');
-          if (ok) {
-            tasks.deleteTask(id);
-            console.log('Tarea borrada');
-          }
-        }
-        break;
-    }
+          case '6': // Borrar
+              const id = await listadoTareasBorrar( tareas.listadoArr );
+              if ( id !== '0' ) {
+                  const ok = await confirmar('¿Está seguro?');
+                  if ( ok ) {
+                      tareas.borrarTarea( id );
+                      console.log('Tarea borrada');
+                  }
+              }
+          break;
 
-    saveDB(tasks.listArr);
+      }
 
-    await pause();
-  } while (opt !== '0');
 
-  // pause();
-};
+      saveDB( tareas.listadoArr );
+
+      await pausa();
+
+  } while( opt !== '0' );
+
+
+  // pausa();
+
+}
+
 
 main();
